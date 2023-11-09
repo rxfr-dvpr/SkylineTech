17b822040d9cd97bca9d11b1b9b0243203962a11
Avazbek <saidaliyev.avazbek14@gmail.com>
2023-11-09 23:05:49 +0500

    website default language changed

--- src/langs/lang.js
+++ src/langs/lang.js
@@ -2,7 +2,7 @@ import en from "./en.json";
 import ru from "./ru.json";
 import uz from "./uz.json";
 
-export let defaultLocale = 'en'
+export let defaultLocale = 'ru'
 
 export const languages = {
     en,
